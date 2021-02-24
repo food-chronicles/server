5d92aefbb12d469d6005549798333a38892e3a95
Aldyco Turang <aldycoturang@gmail.com>
2021-02-25 01:51:48 +0700

    checkpoint

--- _test_/miscellaneous.test.js
+++ _test_/miscellaneous.test.js
@@ -1,16 +1,33 @@
 const nodemailer = require("nodemailer");
-const { sendKey } = require("../service/mail");
+const { sendKey, retrySending } = require("../service/mail");
 jest.mock("nodemailer");
+jest.useFakeTimers();
 
 describe("Mail Service Test", () => {
   describe("Failed Service Test", () => {
-    it("should return undefined", async (done) => {
-      nodemailer.createTransport.mockResolvedValue(null);
+    it("should failed sending email", async () => {
+      const sendMailMock = jest.fn(() => {
+        return Promise.reject(new Error("mock"));
+      });
+      nodemailer.createTransport.mockReturnValue({
+        sendMail: sendMailMock,
+      });
 
-      const res = await sendKey("user.email", "product.name", "key");
-      expect(res).toBeUndefined();
+      // sendKey("user.email", "product.name", "key");
+      await retrySending({});
+      // expect(retrySending({})).rejects.toThrow("mock");
+      jest.runAllTimers();
+      // function flushPromises() {
+      //   return new Promise((resolve) => setImmediate(resolve));
+      // }
 
-      done();
+      // jest.advanceTimersByTime(1000000);
+      // await flushPromises();
+      expect(sendMailMock).toHaveBeenCalledTimes(1);
+      jest.runOnlyPendingTimers();
+
+      expect(setTimeout).toHaveBeenCalledTimes(2);
+      // expect(res).toBeUndefined();
     });
   });
 });
